baa4c446bfa3c9665d55b4bb1f68d8fcc488a8eb
Ahmed <43687458+AhmedZaki918@users.noreply.github.com>
2018-10-30 15:04:52 +0200

    Biggest update
    Update the whole project and it contains RadioButtons, CheckBoxes and EditText instead of Buttons to displays the choices

--- app/src/main/java/com/example/android/sportquiz/MainActivity.java
+++ app/src/main/java/com/example/android/sportquiz/MainActivity.java
@@ -7,23 +7,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    //Variables declarations.
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Create object of class Questions.
     Questions que = new Questions();
+
+    //Variables declarations.
+    private LinearLayout liCheckBox;
+    private RadioGroup rg;
+    private EditText answerEditText;
     private TextView Question;
-    private TextView Que;
+    private TextView queNumber;
     private TextView scoreDisplay;
     private int score = 0;
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btn4;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private CheckBox ch1;
+    private CheckBox ch2;
+    private CheckBox ch3;
+    private CheckBox ch4;
     private Button next;
+
     //This variable gets requested question from mQuestions array in Questions class.
     private int mQuestionNumber = 0;
     private int QueScore10 = 0;
@@ -37,49 +50,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Find locations of views.
-        btn1 = findViewById(R.id.Answer1_Button);
-        btn2 = findViewById(R.id.Answer2_Button);
-        btn3 = findViewById(R.id.Answer3_Button);
-        btn4 = findViewById(R.id.Answer4_Button);
+        rg = findViewById(R.id.RadioGroup);
+        rb1 = findViewById(R.id.Choice1_RadioButton);
+        rb2 = findViewById(R.id.Choice2_RadioButton);
+        rb3 = findViewById(R.id.Choice3_RadioButton);
+        rb4 = findViewById(R.id.Choice4_RadioButton);
+        ch1 = findViewById(R.id.Choice1_CheckBox);
+        ch2 = findViewById(R.id.Choice2_CheckBox);
+        ch3 = findViewById(R.id.Choice3_CheckBox);
+        ch4 = findViewById(R.id.Choice4_CheckBox);
+        answerEditText = findViewById(R.id.Answer_EditText);
         scoreDisplay = findViewById(R.id.Score_TextView);
         next = findViewById(R.id.Next_Button);
         Question = findViewById(R.id.Question_TextView);
-        Que = findViewById(R.id.Que_TextView);
+        queNumber = findViewById(R.id.QueNumber_TextView);
+        liCheckBox = findViewById(R.id.LineraLayout_CheckBox);
 
         //Displays the questions and chocies on screen at first time only.
         updateQuestion();
 
-        //This button A called when the user click on it.
-        btn1.setOnClickListener(new View.OnClickListener() {
+        //This RadioButton 1 called when the user click on it.
+        rb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (btn1.getText() == answer) {
-
-                    btn1.setBackgroundColor(getResources().getColor(R.color.True));
+                if (rb1.getText() == answer) {
+                    rb1.setBackgroundColor(getResources().getColor(R.color.True));
                     displayScore();
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
-
                 } else {
-
-                    btn1.setBackgroundColor(getResources().getColor(R.color.False));
+                    rb1.setBackgroundColor(getResources().getColor(R.color.False));
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
+                    //Displays the ResultActivity.
                     Intent i = new Intent(MainActivity.this, ResultActivity.class);
                     i.putExtra("Score", score + " / 10");
                     startActivity(i);
@@ -87,38 +105,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //This button B called when the user click on it.
-        btn2.setOnClickListener(new View.OnClickListener() {
+        //This RadioButton 2 called when the user click on it.
+        rb2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-                if (btn2.getText() == answer) {
-
-                    btn2.setBackgroundColor(getResources().getColor(R.color.True));
+                if (rb2.getText() == answer) {
+                    rb2.setBackgroundColor(getResources().getColor(R.color.True));
                     displayScore();
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
-
                 } else {
-
-                    btn2.setBackgroundColor(getResources().getColor(R.color.False));
+                    rb2.setBackgroundColor(getResources().getColor(R.color.False));
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
+                    //Displays the ResultActivity.
                     Intent i = new Intent(MainActivity.this, ResultActivity.class);
                     i.putExtra("Score", score + " / 10");
                     startActivity(i);
@@ -126,35 +141,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //This button C called when the user click on it.
-        btn3.setOnClickListener(new View.OnClickListener() {
+        //This RadioButton 3 called when the user click on it.
+        rb3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (btn3.getText() == answer) {
-                    btn3.setBackgroundColor(getResources().getColor(R.color.True));
+                if (rb3.getText() == answer) {
+                    rb3.setBackgroundColor(getResources().getColor(R.color.True));
                     displayScore();
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
                 } else {
-
-                    btn3.setBackgroundColor(getResources().getColor(R.color.False));
+                    rb3.setBackgroundColor(getResources().getColor(R.color.False));
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
+                    //Displays the ResultActivity.
                     Intent i = new Intent(MainActivity.this, ResultActivity.class);
                     i.putExtra("Score", score + " / 10");
                     startActivity(i);
@@ -162,47 +177,84 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //This button D called when the user click on it.
-        btn4.setOnClickListener(new View.OnClickListener() {
+        //This RadioButton 4 called when the user click on it.
+        rb4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (btn4.getText() == answer) {
-
-                    btn4.setBackgroundColor(getResources().getColor(R.color.True));
+                if (rb4.getText() == answer) {
+                    rb4.setBackgroundColor(getResources().getColor(R.color.True));
                     displayScore();
 
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
                 } else {
-
-                    btn4.setBackgroundColor(getResources().getColor(R.color.False));
+                    rb4.setBackgroundColor(getResources().getColor(R.color.False));
                     //Check if this the last question or not before update question.
                     if (mQuestionNumber == que.mQuestions.length) {
 
+                        //Displays the ResultActivity.
                         Intent i = new Intent(MainActivity.this, ResultActivity.class);
                         i.putExtra("Score", score + " / 10");
                         startActivity(i);
                     }
-
+                    //Displays the ResultActivity.
                     Intent i = new Intent(MainActivity.this, ResultActivity.class);
                     i.putExtra("Score", score + " / 10");
                     startActivity(i);
                 }
             }
         });
+
+        //Check if the user enter the right answer on EditText or not.
+        answerEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Convert EditText to String and store it in  String variable.
+                String value = answerEditText.getText().toString();
+
+                //Convert String variable to Integer and store it in Integer variable.
+                int finalValue = Integer.parseInt(value);
+
+                //Check if the answer = 5, displays the score and if not update question only without update the score.
+                if (finalValue == 5) {
+                    displayScore();
+                } else {
+                    //Displays the ResultActivity.
+                    Intent i = new Intent(MainActivity.this, ResultActivity.class);
+                    i.putExtra("Score", score + " / 10");
+                    startActivity(i);
+                }
+            }
+        });
+
+        //Set one onClickListner for all checkboxes.
+        ch1.setOnClickListener(this);
+        ch2.setOnClickListener(this);
+        ch3.setOnClickListener(this);
+        ch4.setOnClickListener(this);
 
         //This next button displays next question.
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updateQuestion();
+
+                if (mQuestionNumber == que.mQuestions.length) {
+
+                    //Displays the ResultActivity.
+                    Intent i = new Intent(MainActivity.this, ResultActivity.class);
+                    i.putExtra("Score", score + " / 10");
+                    startActivity(i);
+                } else {
+                    updateQuestion();
+                }
             }
         });
     }
@@ -210,51 +262,117 @@ public class MainActivity extends AppCompatActivity {
     //This method update questions.
     private void updateQuestion() {
 
-        QueScore10++;
-        Que.setText("Que: " + QueScore10 + " / 10");
-        resetColors();
-        setMultiAttributes();
-        Question.setText(que.mQuestions[mQuestionNumber]);
-        btn1.setText(que.mChoices[mQuestionNumber][0]);
-        btn2.setText(que.mChoices[mQuestionNumber][1]);
-        btn3.setText(que.mChoices[mQuestionNumber][2]);
-        btn4.setText(que.mChoices[mQuestionNumber][3]);
-        answer = que.mAnswer[mQuestionNumber];
-        mQuestionNumber++;
+        //Check if current index of question is greater than one, switch EditText with RadioButtons.
+        if (mQuestionNumber > 1) {
+            rg.setVisibility(View.VISIBLE);
+            answerEditText.setVisibility(View.GONE);
+        }
+
+        //Check if current index of question equal to one, switch RadioButtons with EditText.
+        if (mQuestionNumber == 1) {
+            rg.setVisibility(View.GONE);
+            answerEditText.setVisibility(View.VISIBLE);
+            Question.setText(que.mQuestions[mQuestionNumber]);
+        }
+
+        //Check if current index of question equal to nine or the last question, switch RadioButtons with CheckBoxes.
+        if (mQuestionNumber == 9) {
+            QueScore10++;
+            queNumber.setText("Que: " + QueScore10 + " / 10");
+            next.setText("Finish");
+            rg.setVisibility(View.GONE);
+            liCheckBox.setVisibility(View.VISIBLE);
+
+            //Displays the question.
+            Question.setText(que.mQuestions[mQuestionNumber]);
+
+            //Displays four choices of the question.
+            ch1.setText(que.mChoices[mQuestionNumber][0]);
+            ch2.setText(que.mChoices[mQuestionNumber][1]);
+            ch3.setText(que.mChoices[mQuestionNumber][2]);
+            ch4.setText(que.mChoices[mQuestionNumber][3]);
+            answer = que.mAnswer[mQuestionNumber];
+            setMultiAttributesCheckBoxes();
+            mQuestionNumber++;
+
+        } else {
+            QueScore10++;
+            queNumber.setText("Que: " + QueScore10 + " / 10");
+            resetColors();
+            setMultiAttributes();
+            uncheckSelection();
+            Question.setText(que.mQuestions[mQuestionNumber]);
+            rb1.setText(que.mChoices[mQuestionNumber][0]);
+            rb2.setText(que.mChoices[mQuestionNumber][1]);
+            rb3.setText(que.mChoices[mQuestionNumber][2]);
+            rb4.setText(que.mChoices[mQuestionNumber][3]);
+            answer = que.mAnswer[mQuestionNumber];
+            mQuestionNumber++;
+        }
     }
 
-    //This method reset colors for all buttons to default color.
+    //This method reset colors for all views to default color.
     private void resetColors() {
-
-        btn1.setBackgroundResource(R.color.Buttons);
-        btn2.setBackgroundResource(R.color.Buttons);
-        btn3.setBackgroundResource(R.color.Buttons);
-        btn4.setBackgroundResource(R.color.Buttons);
-
+        rb1.setBackgroundResource(R.color.Buttons);
+        rb2.setBackgroundResource(R.color.Buttons);
+        rb3.setBackgroundResource(R.color.Buttons);
+        rb4.setBackgroundResource(R.color.Buttons);
     }
 
     //This method displays the score.
     private void displayScore() {
-
         score++;
         scoreDisplay.setText("Score : " + score);
     }
 
-    //This method set multi attributes of buttons and question.
+    //This method set multi attributes of views and question.
     private void setMultiAttributes() {
-
         Question.setTextSize(25);
-        btn1.setTextSize(20);
-        btn2.setTextSize(20);
-        btn3.setTextSize(20);
-        btn4.setTextSize(20);
-        btn1.setAllCaps(false);
-        btn2.setAllCaps(false);
-        btn3.setAllCaps(false);
-        btn4.setAllCaps(false);
-        btn1.setTypeface(Typeface.MONOSPACE);
-        btn2.setTypeface(Typeface.MONOSPACE);
-        btn3.setTypeface(Typeface.MONOSPACE);
-        btn4.setTypeface(Typeface.MONOSPACE);
+        rb1.setTextSize(20);
+        rb2.setTextSize(20);
+        rb3.setTextSize(20);
+        rb4.setTextSize(20);
+        rb1.setAllCaps(false);
+        rb2.setAllCaps(false);
+        rb3.setAllCaps(false);
+        rb4.setAllCaps(false);
+        rb1.setTypeface(Typeface.MONOSPACE);
+        rb2.setTypeface(Typeface.MONOSPACE);
+        rb3.setTypeface(Typeface.MONOSPACE);
+        rb4.setTypeface(Typeface.MONOSPACE);
+    }
+
+    //This method set multi attributes of CheckBoxes and question.
+    private void setMultiAttributesCheckBoxes() {
+        Question.setTextSize(25);
+        ch1.setTextSize(20);
+        ch2.setTextSize(20);
+        ch3.setTextSize(20);
+        ch4.setTextSize(20);
+        ch1.setAllCaps(false);
+        ch2.setAllCaps(false);
+        ch3.setAllCaps(false);
+        ch4.setAllCaps(false);
+        ch1.setTypeface(Typeface.MONOSPACE);
+        ch2.setTypeface(Typeface.MONOSPACE);
+        ch3.setTypeface(Typeface.MONOSPACE);
+        ch4.setTypeface(Typeface.MONOSPACE);
+        ch1.setBackgroundResource(R.color.Buttons);
+        ch2.setBackgroundResource(R.color.Buttons);
+        ch3.setBackgroundResource(R.color.Buttons);
+        ch4.setBackgroundResource(R.color.Buttons);
+    }
+
+    //Uncheck the selections of RadioButtons.
+    private void uncheckSelection() {
+        rg.clearCheck();
+    }
+
+    //Check if the user enter right answers or not.
+    @Override
+    public void onClick(View view) {
+        if (ch1.isChecked() && !ch2.isChecked() && ch3.isChecked() && ch4.isChecked()) {
+            score++;
+        }
     }
 }
--- app/src/main/java/com/example/android/sportquiz/Questions.java
+++ app/src/main/java/com/example/android/sportquiz/Questions.java
@@ -2,19 +2,17 @@ package com.example.android.sportquiz;
 
 public class Questions {
 
-
-
     //String array of the questions.
     public String[] mQuestions = {"Who is highest goalscorer in world cup football history ?",
             "How many times has Brazil won the FIFA World Cup ?",
-            "What's the best rank did El Ahly club achieve in clubs world cup ? ?",
+            "What's the best rank did El Ahly club achieve in clubs world cup ?",
             "Who is the current technical manager for Italian Napoli club ?",
             "How many times did Egypt's first team participate in world cup ?",
             "Who is the hero of English Premier League last season ?",
             "What's the nationality of Liverpool player Sadio Mane ?",
             "Where does Belgian striker Lucaco play ?",
             "Who is the best player in the 2018 World Cup ?",
-            "Which club did win the most of Champions League ?"};
+            "Which three clubs of the following Pep Guardiola trains ?"};
 
     //String array of the choices.
     public String[][] mChoices = {{"Miroslav Klose", "Ronaldo", "Maradona", "Pele"},
@@ -26,8 +24,8 @@ public class Questions {
             {"Cameroonian", "Senegalese", "Malian", "South African"},
             {"Chelsea", "Arsenal", "Manchester United", "Liverpool FC"},
             {"Nimar", "Luka Modric", "Eden Hazard", "Antoine Griezmann"},
-            {"Juventus", "Barcelona", "Real Madrid", "AC Milan"}};
+            {"Manchester City", "Barcelona", "FC Bayern Munich", "Manchester United"}};
 
     //String array of answers.
-    public String[] mAnswer = {"Miroslav Klose", "5", "Third rank", "Carlo Ancelotti", "3", "Manchester City", "Senegalese", "Manchester United", "Luka Modric", "Real Madrid"};
+    public String[] mAnswer = {"Miroslav Klose", "5", "Third rank", "Carlo Ancelotti", "3", "Manchester City", "Senegalese", "Manchester United", "Luka Modric","Manchester City" + "Barcelona" + "FC Bayern Munich"};
 }
